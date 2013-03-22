822d1bcb00a853955da98208e904749b0ee66b51
Daniel Jost <djost@mines.edu>
2013-03-21 18:27:32 -0600

    Added config files and Solution.java (That we forgot to add before)

---
+++ ClueBoardTwo/src/board/Solution.java
@@ -0,0 +1,32 @@
+package board;
+
+public class Solution {
+
+	String person;
+	String weapon;
+	String room;
+	
+	public Solution() {
+		
+	}
+	
+	public Solution( String person, String weapon, String room ) {
+		this.person = person;
+		this.weapon = weapon;
+		this.room = weapon;
+	}
+
+	public String getPerson() {
+		return person;
+	}
+
+	public String getWeapon() {
+		return weapon;
+	}
+
+	public String getRoom() {
+		return room;
+	}
+	
+	
+}
